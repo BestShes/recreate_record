7396a0f67ff1d9f95e6795b09aabd8cf7e9f9092
BestShes <qkrdngus16@gmail.com>
2017-06-23 23:30:28 +0900

    diary, post, user search fields settings

--- record/diary/views.py
+++ record/diary/views.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
 from .models import Diary
@@ -6,6 +7,8 @@ from .serializers import DiarySerializer
 
 class DiaryViewSet(ModelViewSet):
     serializer_class = DiarySerializer
-    queryset = Diary.objects.all()
+    queryset = Diary.objects.all().order_by('-created_date')
     permission_classes = (permissions.AllowAny,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('title',)
 
