497158e88381f9022df8e3a2a38f7f59dddbdae9
BestShes <qkrdngus16@gmail.com>
2017-09-21 21:08:41 +0900

    회원가입 시 토큰이 발행되고 로그인시 is_active값을 확인하도록 user/serializer.py수정 utils/sendemail.py에 message수정

--- record/utils/sendemail.py
+++ record/utils/sendemail.py
@@ -1,13 +1,16 @@
 from django.core.mail import send_mail
+
 __all__ = (
     'MailTest',
 )
 
+
 class MailTest():
-    def certification_mail(email):
+    @staticmethod
+    def certification_mail(email, message):
         send_mail(
             'Subject',
-            'Message',
+            'http://localhost:8000/?key=' + message,
             'record@record.com',
             [email],
             fail_silently=False
