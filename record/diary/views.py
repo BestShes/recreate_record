9145679d6eeab97900673c550785b268c55f9974
BestShes <qkrdngus16@gmail.com>
2017-09-27 23:15:42 +0900

    권한을 IsAuthenticatedOrReadOnly로 변경 다이어리 생성시 request값도 같이 넘기도록 작성

--- record/diary/views.py
+++ record/diary/views.py
@@ -1,5 +1,7 @@
 from rest_framework import filters
 from rest_framework import permissions
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from .models import Diary
 from .serializers import DiarySerializer
@@ -8,7 +10,14 @@ from .serializers import DiarySerializer
 class DiaryViewSet(ModelViewSet):
     serializer_class = DiarySerializer
     queryset = Diary.objects.all().order_by('-created_date')
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('title',)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
