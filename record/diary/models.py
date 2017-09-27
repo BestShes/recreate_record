1b60372f6c0556cff221e472c98f9d4df5feb811
BestShes <qkrdngus16@gmail.com>
2017-09-27 15:00:28 +0900

    Diary모델 필드 변경 및 추가

--- record/diary/models.py
+++ record/diary/models.py
@@ -3,8 +3,19 @@ from django.db import models
 
 
 class Diary(models.Model):
+    TRAVEL_AREA = (
+        ('EU', 'Europe'),
+        ('NA', 'North America'),
+        ('SA', 'South America'),
+        ('KR', 'Korea'),
+        ('ASIA', 'Asia'),
+        ('OCE', 'Oceania'),
+    )
     author = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=50)
     cover_img = models.ImageField(blank=True)
+    travel_area = models.CharField(choices=TRAVEL_AREA, max_length=17)
+    travel_start_date = models.DateTimeField(null=True)
+    travel_end_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
